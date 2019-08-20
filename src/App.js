602f425362839095bd9a071fa947e9d5f606bcbc
alvondirlj <a.junner@gmail.com>
2019-08-20 00:13:36 -0300

    CRUD Genres complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Header from './header'
 import Generos from './Generos'
 import NovoGenero from './NovoGenero'
+import EditarGenero from './EditarGenero'
 
 import axios from 'axios'
 import {
@@ -25,6 +26,7 @@ function App() {
       <div >
         <Header />
         <Route path='/' exact component={Home} />
+        <Route path='/generos/:id' exact component={EditarGenero} />
         <Route path='/generos/novo' exact component={NovoGenero} />
         <Route path='/generos' exact component={Generos} />
         <pre>{JSON.stringify(data)}</pre>
